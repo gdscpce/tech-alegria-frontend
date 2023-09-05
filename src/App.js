05a253180aef1485f28c570c7a36dfdaba2a08ff
Sumant-Testbook <sumant.dusane1966@gmail.com>
2023-09-06 00:54:24 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Landing from './components/Landing/Landing';
+import Router from './router/Router';
 import './style.scss';
 
 function App() {
   return (
     <>
+    <Router />
     </>
   );
 }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./landing.scss";
+
+export default function 
+() {
+  return (
+    <div className='parent'>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
