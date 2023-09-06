4ce84438c51d8f1180e152f13aad922871f8a6ee
Sumant-Testbook <sumant.dusane1966@gmail.com>
2023-09-06 11:04:17 +0530

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='footer__title'>TechAlegria X GDSC</div>
+      <div className="footer__year">&copy; 2023</div>
+    </footer>
+  )
+}
